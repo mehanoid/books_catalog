212bc078c79de78f60417c46ed65571eb05a366e
Oleg Grigoriev <mehanoid.ru@gmail.com>
2014-07-11 18:27:10 +0400

    Database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+catalog = {
+    'Александр Сергеевич Пушкин' => ['Капитанская дочка', 'Сказка о царе Салтане'],
+    'Терри Пратчетт' => ['Цвет волшебства', 'Ночная стража', 'Последний герой', 'Опочтарение'],
+    'Айзек Азимов' => ['Я, робот', 'Стальные пещеры', 'Роботы и Империя', 'Академия'],
+    'Филип Пулман' => ['Северное сияние', 'Чудесный нож', 'Янтарный телескоп']
+}
+
+catalog.each do |author_name, books|
+  author = Author.create name: author_name
+  books.each do |book_title|
+    author.books.create(title: book_title)
+  end
+end
